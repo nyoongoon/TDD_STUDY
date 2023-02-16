7b2ef7acf57954a82db91de138a549e67f636d34
nyoongoon <nyoongoon@naver.com>
2023-02-16 20:16:33 +0900

    JUnit5에서 @Before가 아니라 @BeforeEach

--- src/test/java/com/test/junit/ProfileTest.java
+++ src/test/java/com/test/junit/ProfileTest.java
@@ -1,19 +1,31 @@
 package com.test.junit;
 
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProfileTest {
+    private Profile profile;
+    private Question question;
+    private Criteria criteria;
+
+    @BeforeEach
+    public void create(){
+        profile = new Profile("Bull Hockey, Inc.");
+        question = new BooleanQuestion(1, "Got bonuses?");
+        criteria = new Criteria();
+    }
+
+
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet(){
         //given
-        Profile profile = new Profile("Bully Hockey, Inc.");
-        Question question = new BooleanQuestion(1, "Got bonuses?");
         Answer profileAnswer = new Answer(question, Bool.FALSE); // 보너스 안줌
         profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE); // 보너스 필요
         Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch); //중요한 질문
         criteria.add(criterion);
@@ -26,11 +38,8 @@ class ProfileTest {
     @Test
     public void matchAnswersTrueForAnyDontCareCriteria(){
         //given
-        Profile profile = new Profile("Bully Hockey, Inc.");
-        Question question = new BooleanQuestion(1, "Got milk?");
         Answer profileAnswer = new Answer(question, Bool.FALSE);
         profile.add(profileAnswer);
-        Criteria criteria = new Criteria();
         Answer criteriaAnswer = new Answer(question, Bool.TRUE);
         Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
         criteria.add(criterion);
